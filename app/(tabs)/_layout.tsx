5acf3d1aa06a2611daa3aa150e0794fa6f4cfeec
Younghyun <yhbae97@gmail.com>
2024-10-21 22:03:49 +0200

    Style Tabs

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,41 +1,49 @@
 import { Stack, Tabs } from 'expo-router';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faBookmark, faMessage, faUser } from '@fortawesome/free-regular-svg-icons';
+import { faGlobe, faHouse } from '@fortawesome/free-solid-svg-icons';
 
 const HomeLayout = () => {
   return (
-    <Tabs>
+    <Tabs screenOptions={{ tabBarActiveTintColor: '#B8ADE0' }}>
       <Tabs.Screen
         name="tab_1"
         options={{
           headerTitle: "Bookmarks",
-          title: "Bookmarks"
+          title: "Bookmarks",
+          tabBarIcon: ({ color }) => <FontAwesomeIcon size={16} icon={faBookmark} color={color} />,
         }}
       />
       <Tabs.Screen
         name="tab_2"
         options={{
-          headerTitle: "Exlore",
-          title: "Explore"
+          headerTitle: "Explore",
+          title: "Explore",
+          tabBarIcon: ({ color }) => <FontAwesomeIcon size={16} icon={faGlobe} color={color} />,
         }}
       />
       <Tabs.Screen
         name="index"
         options={{
           headerTitle: "Home",
-          title: "Home"
+          title: "Home",
+          tabBarIcon: ({ color }) => <FontAwesomeIcon size={16} icon={faHouse} color={color} />,
         }}
       />
       <Tabs.Screen
         name="tab_3"
         options={{
           headerTitle: "Chat",
-          title: "Chat"
+          title: "Chat",
+          tabBarIcon: ({ color }) => <FontAwesomeIcon size={16} icon={faMessage} color={color} />,
         }}
       />
       <Tabs.Screen
         name="tab_4"
         options={{
           headerTitle: "My Profile",
-          title: "My"
+          title: "My",
+          tabBarIcon: ({ color }) => <FontAwesomeIcon size={16} icon={faUser} color={color} />,
         }}
       />
     </Tabs>
