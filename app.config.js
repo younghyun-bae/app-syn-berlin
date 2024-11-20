fd3f79b2f829e06eea27bd08e36caa95ac937e00
Younghyun <yhbae97@gmail.com>
2024-11-20 15:57:21 +0100

    Update google-services.json and project configurations

--- app.config.js
+++ app.config.js
@@ -1,13 +1,11 @@
-import 'dotenv/config';
-
 export default {
   "expo": {
-    "name": "app-syn-berlin",
+    "name": "SYN",
     "slug": "app-syn-berlin",
-    "scheme": ["app-syn-berlin"],
+    "scheme": "app-syn-berlin",
     "facebookScheme": "fb1038031424464248",
     "facebookDisplayName": "SYN",
-    "facebookAppId": process.env.EXPO_PUBLIC_FACEBOOK_APP_ID,
+    "facebookAppId": "1038031424464248",
     "version": "1.0.0",
     "orientation": "portrait",
     "icon": "./assets/icon.png",
@@ -17,6 +15,7 @@ export default {
       "resizeMode": "contain",
       "backgroundColor": "#ffffff"
     },
+    "jsEngine": "jsc",
     "platforms": [
       "ios",
       "android"
@@ -24,11 +23,34 @@ export default {
     "ios": {
       "bundleIdentifier": "com.bae.syn",
       "googleServicesFile": "./GoogleService-Info.plist",
-      "supportsTablet": true
+      "supportsTablet": true,
+      "infoPlist": {
+        "NSCameraUsageDescription": "This app requires camera access.",
+        "NSPhotoLibraryUsageDescription": "This app requires photo library access."
+      }
     },
     "android": {
       "package": "com.bae.syn",
-      "googleServicesFile": "google-services.json",
+      "googleServicesFile": process.env.GOOGLE_SERVICES_FILE_ANDROID,
+      "intentFilters": [
+        {
+          "action": "VIEW",
+          "autoVerify": true,
+          "data": [
+            {
+              "scheme": "fb1038031424464248",
+              "host": "authorize"
+            }
+          ],
+          "category": ["BROWSABLE", "DEFAULT"]
+        }
+      ],
+      "permissions": [
+        "CAMERA",
+        "READ_EXTERNAL_STORAGE",
+        "READ_MEDIA_IMAGES",
+        "INTERNET"
+      ],
       "adaptiveIcon": {
         "foregroundImage": "./assets/adaptive-icon.png",
         "backgroundColor": "#ffffff"
@@ -41,7 +63,15 @@ export default {
     "plugins": [
       "expo-router",
       "expo-font",
-      "@react-native-google-signin/google-signin"
+      "@react-native-google-signin/google-signin",
+      "react-native-fbsdk-next",
+      "expo-image-picker",
+      [
+        "expo-location",
+        {
+          "locationAlwaysAndWhenInUsePermission": "Allow $(PRODUCT_NAME) to use your location."
+        }
+      ],
     ],
     "extra": {
       "eas": {
