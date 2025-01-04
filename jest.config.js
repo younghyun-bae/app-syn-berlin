f81db1caef07fb41f166b5fa5410ee6abbd8b2cf
Younghyun <yhbae97@gmail.com>
2025-01-04 17:05:47 +0100

    test: Set up Jest unit testing environment and Add dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,16 @@
 module.exports = {
+  setupFiles: ['./jest.setup.js'],
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation)',
+    'node_modules/(?!(jest-)?react-native|@react-native|@react-navigation|firebase|@fortawesome/react-native-fontawesome)',
   ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '\\.svg': '<rootDir>/__mocks__/svgMock.js'
+  },
+  testEnvironment: 'node',
 };
