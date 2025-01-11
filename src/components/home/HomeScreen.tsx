c05938b6d611b78ed9907113d472205d06816cd0
Younghyun <yhbae97@gmail.com>
2025-01-11 21:42:13 +0100

    refactor: Modularize HomeScreen interfaces, and data for reusability

--- src/components/home/HomeScreen.tsx
+++ src/components/home/HomeScreen.tsx
@@ -6,34 +6,7 @@ import EventList from './EventList';
 import PopularList from './PopularList';
 import AboutSection from './AboutSection';
 
-interface Event {
-    id: string;
-    image: any;
-    description: string;
-}
-
-interface PopularItem {
-    id: string;
-    title: string;
-    link?: string;
-}
-
-const events: Event[] = [
-    { id: '1', image: require('../../../assets/images/SYN_lime.png'), description: 'Event Advertisement here' },
-    { id: '2', image: require('../../../assets/images/SYN_black.png'), description: 'Event 2 Advertisement' },
-    { id: '3', image: require('../../../assets/images/SYN_lime.png'), description: 'Event 3 Advertisement' },
-    { id: '4', image: require('../../../assets/images/SYN_black.png'), description: 'Event 4 Advertisement' },
-    { id: '5', image: require('../../../assets/images/SYN_lime.png'), description: 'Event 5 Advertisement' },
-];
-
-const popularItems: PopularItem[] = [
-    { id: '1', title: 'Hiring', link: '' },
-    { id: '2', title: '1 : 1\nCollaboration' },
-    { id: '3', title: 'Threads' },
-    { id: '4', title: 'Working\nSpace' },
-    { id: '5', title: 'Challenges' },
-    { id: '6', title: 'Meetups' },
-];
+import { events, popularItems } from './data';
 
 const HomeScreen = () => {
     return (
---
+++ src/components/home/data.ts
@@ -0,0 +1,18 @@
+import { Event, PopularItem } from './types';
+
+export const events: Event[] = [
+  { id: '1', image: require('../../../assets/images/SYN_lime.png'), description: 'Event Advertisement here' },
+  { id: '2', image: require('../../../assets/images/SYN_black.png'), description: 'Event 2 Advertisement' },
+  { id: '3', image: require('../../../assets/images/SYN_lime.png'), description: 'Event 3 Advertisement' },
+  { id: '4', image: require('../../../assets/images/SYN_black.png'), description: 'Event 4 Advertisement' },
+  { id: '5', image: require('../../../assets/images/SYN_lime.png'), description: 'Event 5 Advertisement' },
+];
+
+export const popularItems: PopularItem[] = [
+  { id: '1', title: 'Hiring', link: '' },
+  { id: '2', title: '1 : 1\nCollaboration' },
+  { id: '3', title: 'Threads' },
+  { id: '4', title: 'Working\nSpace' },
+  { id: '5', title: 'Challenges' },
+  { id: '6', title: 'Meetups' },
+];
