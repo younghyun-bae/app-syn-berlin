33b5b1c45aa90c059c143b1b314bab4478ba5713
Younghyun <yhbae97@gmail.com>
2025-01-24 22:34:03 +0100

    feat: Style Explore and EachUser screen

--- src/components/explore/EachUserScreen.tsx
+++ src/components/explore/EachUserScreen.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, Button, TouchableOpacity, Modal } from 'react-native';
+import { Button, Modal } from 'react-native';
 import { useRouter, useLocalSearchParams } from 'expo-router';
 import { useUsers } from 'src/api/context/UsersContext';
-
 import styled from 'styled-components/native';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-regular-svg-icons';
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import ChatRequestScreen from './ChatRequestScreen';
 
 const EachUserScreen: React.FC = () => {
@@ -22,33 +22,52 @@ const EachUserScreen: React.FC = () => {
 
   if (!user) {
     return (
-      <View style={styles.container}>
-        <Text style={styles.errorText}>User not found!</Text>
+      <EachUserContainer>
+        <ErrorText>User not found!</ErrorText>
         <Button title="Back to Explore" onPress={() => router.back()} />
-      </View>
+      </EachUserContainer>
     );
   }
 
-
   return (
-    <>
-      <ScrollView contentContainerStyle={styles.container}>
-        <Text style={styles.displayName}>{user.displayName || "N/A"}</Text>
-        <ContentText>{user.location || "Unknown"}</ContentText>
-        <Text style={styles.jobTitle}>{user.jobTitle || "Not provided"}</Text>
-        <ContentText>{user.mainField || "Not specified"}</ContentText>
-        <SectionTitle>Interest</SectionTitle>
-        <SectionTitle>About Me</SectionTitle>
-        <ContentText>{user.aboutMe || "No information available"}</ContentText>
-        <SectionTitle>Proud Work</SectionTitle>
-        <ContentText>{user.proudWork || "No details"}</ContentText>
-        <SectionTitle>Portfolio</SectionTitle>
-        <ContentText>{user.portfolio || "Not available"}</ContentText>
-        <SectionTitle>Languages</SectionTitle>
-        <ContentText>{user.languages || "No data"}</ContentText>
-        <TouchableOpacity></TouchableOpacity>
+    <EachUserContainer>
+      <ScrollContainer>
+        <ProfileImage source={{ uri: user.profilePic }} />
+        <Header>
+          <DisplayName>{user.displayName || "N/A"}</DisplayName>
+          <LocationContainer>
+            <FontAwesomeIcon icon={faLocationDot} color="#232323" size={15} />
+            <ContentText>{user.location || "Unknown"}</ContentText>
+          </LocationContainer>
+        </Header>
+        <JobTitle>{user.jobTitle || "Not provided"}</JobTitle>
+        <MainField>{user.mainField || "Not specified"}</MainField>
+        <SectionContainer>
+          <SectionTitle>Interest</SectionTitle>
+          <InterestsContainer>
+            {user.interests?.length ? user.interests.map((interest, index) => (
+              <InterestTag key={index}><InterestTagText>{interest}</InterestTagText></InterestTag>
+            )) : <ContentText>No Interests Selected</ContentText>}
+          </InterestsContainer>
+        </SectionContainer>
+        <SectionContainer>
+          <SectionTitle>About Me</SectionTitle>
+          <ContentText>{user.aboutMe || "No information available"}</ContentText>
+        </SectionContainer>
+        <SectionContainer>
+          <SectionTitle>Proud Work</SectionTitle>
+          <ContentText>{user.proudWork || "No details"}</ContentText>
+        </SectionContainer>
+        <SectionContainer>
+          <SectionTitle>Portfolio</SectionTitle>
+          <ContentText>{user.portfolio || "Not available"}</ContentText>
+        </SectionContainer>
+        <SectionContainer>
+          <SectionTitle>Languages</SectionTitle>
+          <ContentText>{user.languages || "No data"}</ContentText>
+        </SectionContainer>
         <Button title="Back to Explore" onPress={() => router.back()} />
-      </ScrollView>
+      </ScrollContainer>
       <RequestBtn onPress={() => setModalVisible(true)} testID="request-btn">
         <FontAwesomeIcon icon={faPaperPlane} color="#FFFFFF" size={25} />
       </RequestBtn>
@@ -58,29 +77,105 @@ const EachUserScreen: React.FC = () => {
         visible={modalVisible}
         onRequestClose={() => setModalVisible(false)}
       >
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'rgba(0, 0, 0, 0.5)' }}>
+        <ModalContainer>
           <ChatRequestScreen 
             onClose={handleModalClose}
             recieverName={user.displayName}
             eachUserUid={user.uid}
           />
-        </View>
+        </ModalContainer>
       </Modal>
-    </>
+    </EachUserContainer>
   );
 };
 
+const EachUserContainer = styled.View`
+  flex: 1;
+  padding: 25px;
+  background-color: #FFF;
+`;
+
+const ScrollContainer = styled.ScrollView`
+  flex: 1;
+`;
+
+const ProfileImage = styled.Image`
+  width: 100%;
+  height: 200px;
+  border-radius: 30px;
+  margin-bottom: 20px;
+`;
+
+const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+const DisplayName = styled.Text`
+  font-family: 'DMSans_700Bold';
+  font-size: 24px;
+  color: #232323;
+`;
+
+const LocationContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const JobTitle = styled.Text`
+  font-family: 'DMSans_500Medium';
+  font-size: 18px;
+  color: #232323;
+  margin-bottom: 5px;
+`;
+
+const MainField = styled.Text`
+  font-family: 'DMSans_500Medium';
+  font-size: 18px;
+  color: #232323;
+  margin-bottom: 15px;
+`;
+
+const SectionContainer = styled.View`
+  background-color: rgba(229, 229, 229, 0.2);
+  border-radius: 10px;
+  padding: 10px;
+  margin-bottom: 15px;
+`;
+
 const SectionTitle = styled.Text`
-    font-family: 'DMSans_700Bold';
-    font-size: 18px;
-    color: #232323;
-    margin-top: 10px;
+  font-family: 'DMSans_700Bold';
+  font-size: 18px;
+  color: #232323;
+  margin-bottom: 5px;
 `;
 
 const ContentText = styled.Text`
   font-family: 'DMSans_500Medium';
   font-size: 14px;
   color: #5A5A5F;
+`;
+
+const InterestsContainer = styled.View`
+  flex-direction: row;
+  flex-wrap: wrap;
+  margin-top: 5px;
+`;
+
+const InterestTag = styled.View`
+  background-color: #FFF;
+  border: 1px solid #000;
+  border-radius: 25px;
+  padding: 5px 10px;
+  margin-right: 5px;
+  margin-bottom: 5px;
+`;
+
+const InterestTagText = styled.Text`
+  color: #232323;
+  font-family: 'DMSans_300Regular';
 `;
 
 const RequestBtn = styled.TouchableOpacity`
@@ -92,24 +187,16 @@ const RequestBtn = styled.TouchableOpacity`
   border-radius: 50px;
 `;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-  errorText: {
-    fontSize: 18,
-    color: 'red',
-  },
-  displayName: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  jobTitle: {
-    fontSize: 18,
-    color: '#666',
-    marginBottom: 16,
-  },
-});
+const ModalContainer = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const ErrorText = styled.Text`
+  font-size: 18px;
+  color: red;
+`;
 
 export default EachUserScreen;
--- src/components/explore/ExploreScreen.tsx
+++ src/components/explore/ExploreScreen.tsx
@@ -1,34 +1,53 @@
-import React from 'react';
-import { FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { FlatList, StyleSheet, View, Text, ImageBackground, Pressable } from 'react-native';
 import { useUsers } from 'src/api/context/UsersContext';
-
 import styled from 'styled-components/native';
-
-import { useRouter } from 'expo-router'; 
+import { useRouter } from 'expo-router';
 
 const ExploreScreen: React.FC = () => {
   const { users } = useUsers().state;
-
   const router = useRouter();
+  const [selectedUser, setSelectedUser] = useState<string | null>(null);
 
+  const handlePressIn = (uid: string) => {
+    setSelectedUser(uid);
+  };
+
+  const handlePressOut = () => {
+    setSelectedUser(null);
+  };
 
   return (
     <ExploreContainer>
       <FlatList
         data={users}
+        numColumns={2}
         keyExtractor={(item) => item.uid}
         renderItem={({ item }) => (
-          // When it's hover, show AboutMe text
-          <TouchableOpacity
+          <Pressable
             style={styles.userCard}
             onPress={() => router.push(`/tab_2/eachUser?uid=${item.uid}`)}
+            onPressIn={() => handlePressIn(item.uid)}
+            onPressOut={handlePressOut}
           >
-          {/* Background will be each user's profile pic */}
-            <TitleContainer>
-              <UserName>{item.displayName}</UserName>
-              <JobTitle>{item.jobTitle}</JobTitle>
-            </TitleContainer>
-          </TouchableOpacity>
+            <ImageBackground
+              source={{ uri: item.profilePic }} // should be implemented
+              style={styles.imageBackground}
+              imageStyle={styles.imageStyle}
+            >
+              {selectedUser === item.uid && (
+                <AboutMeContainer>
+                  <AboutMeText numberOfLines={12} ellipsizeMode="tail">
+                    {item.aboutMe}
+                  </AboutMeText>
+                </AboutMeContainer>
+              )}
+              <TitleContainer isPressed={selectedUser === item.uid}>
+                <UserName>{item.displayName}</UserName>
+                <JobTitle>{item.jobTitle}</JobTitle>
+              </TitleContainer>
+            </ImageBackground>
+          </Pressable>
         )}
       />
     </ExploreContainer>
@@ -36,43 +55,68 @@ const ExploreScreen: React.FC = () => {
 };
 
 const ExploreContainer = styled.View`
-    flex: 1;
-    padding: 16px;
-    background-color: '#fff';
-`
+  flex: 1;
+  padding: 10px;
+  background-color: #e2e2e2;
+`;
 
-const TitleContainer = styled.View`
-  
+const TitleContainer = styled.View<{ isPressed: boolean }>`
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background-color: ${(props) => props.isPressed ? 'rgba(144, 130, 195, 0.7)' : 'rgba(255, 255, 255, 0.8)'};
+  padding: 4px;
+  align-items: center;
+`;
+
+const AboutMeContainer = styled.View`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 37px;
+  justify-content: flex-start;
+  align-items: flex-start;
+  background-color: rgba(255, 255, 255, 0.9);
+  padding: 6px;
 `;
 
 const UserName = styled.Text`
   font-family: 'DMSans_700Bold';
-  font-size: 16px;
+  font-size: 12px;
   color: #232323;
+  text-align: center;
 `;
 
 const JobTitle = styled.Text`
   font-family: 'DMSans_500Medium';
-  font-size: 12px;
+  font-size: 10px;
   color: #232323;
+  text-align: center;
+`;
+
+const AboutMeText = styled.Text`
+  font-family: 'DMSans_300Regular';
+  font-size: 11px;
+  color: #232323;
+  text-align: center;
+  padding: 7px;
 `;
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 16,
-  },
   userCard: {
-    padding: 16,
-    marginBottom: 12,
-    backgroundColor: '#f9f9f9',
-    borderRadius: 8,
-    elevation: 2,
+    flex: 0.5,
+    margin: 8,
+    aspectRatio: 1,
+    borderRadius: 20,
+    overflow: 'hidden',
   },
-  jobTitle: {
-    fontSize: 14,
-    color: '#666',
+  imageBackground: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  imageStyle: {
+    borderRadius: 20,
   },
 });
 
